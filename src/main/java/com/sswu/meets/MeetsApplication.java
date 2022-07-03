a73057864baf99e39339d40e0aca5de13879924d
yujung7768903 <yu-jung31476@naver.com>
2022-07-03 18:37:00 +0900

    Create Spring project

---
+++ src/main/java/com/sswu/meets/MeetsApplication.java
@@ -0,0 +1,13 @@
+package com.sswu.meets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sswu/meets/MeetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sswu.meets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
