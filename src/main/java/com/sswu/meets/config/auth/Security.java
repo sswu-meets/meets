29342a4e191ab715866d6919f45da4a20ccc81bd
yujung7768903 <yu-jung31476@naver.com>
2022-07-11 16:05:26 +0900

    구글 로그인 구현

--- src/main/java/com/sswu/meets/config/auth/Security.java
+++ src/main/java/com/sswu/meets/config/auth/Security.java
@@ -1,2 +1,33 @@
-package com.sswu.meets.config.auth;public class Security {
+package com.sswu.meets.config.auth;
+
+import com.sswu.meets.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class Security extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/user", "/user/**", "/css/**", "/images/**", "/js/**").permitAll()
+                .antMatchers("/login").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
 }
--- src/main/java/com/sswu/meets/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/sswu/meets/config/auth/dto/OAuthAttributes.java
@@ -1,2 +1,50 @@
-package com.sswu.meets.config.auth.dto;public class OAuthAttributes {
+package com.sswu.meets.config.auth.dto;
+
+import com.sswu.meets.domain.user.Role;
+import com.sswu.meets.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String email;
+    private String name;
+    private String profile_url;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String email, String name, String profile_url) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.email = email;
+        this.name = name;
+        this.profile_url = profile_url;
+    }
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        System.out.println("프로필: " +(String) attributes.get("picture"));
+        return OAuthAttributes.builder()
+                .email((String) attributes.get("email"))
+                .name((String) attributes.get("name"))
+                .profile_url((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .email(email)
+                .name(name)
+                .profile_url(profile_url)
+                .role(Role.USER)
+                .build();
+    }
 }
