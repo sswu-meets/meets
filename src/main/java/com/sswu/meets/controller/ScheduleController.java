325cd23515aff171519a03fafa0f3fae2a64061e
yujung7768903 <yu-jung31476@naver.com>
2022-07-29 17:53:04 +0900

    schedule crud 완성

--- src/main/java/com/sswu/meets/controller/ScheduleController.java
+++ src/main/java/com/sswu/meets/controller/ScheduleController.java
@@ -5,33 +5,47 @@ import com.sswu.meets.dto.ScheduleSaveRequestDto;
 import com.sswu.meets.dto.ScheduleUpdateRequestDto;
 import com.sswu.meets.dto.UserSaveRequestDto;
 import com.sswu.meets.service.ScheduleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "일정")
 @RequiredArgsConstructor
 @RestController
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
-    //일정 등록
+    @ApiOperation(value = "모임에 일정 등록")
+    @ApiImplicitParam(name = "meetingNo", value = "모임 번호")
     @PostMapping("/schedule/{meetingNo}")
     public Long save(@PathVariable Long meetingNo, @RequestBody ScheduleSaveRequestDto scheduleSaveRequestDto) {
         return scheduleService.save(meetingNo, scheduleSaveRequestDto);
     }
 
-    //일정 수정
+    @ApiOperation(value = "모임 일정 조회")
+    @ApiImplicitParam(name = "meetingNo", value = "모임 번호")
+    @GetMapping("/schedule/{meetingNo}")
+    public List<ScheduleResponseDto> getScheduleList(@PathVariable Long meetingNo) {
+        return scheduleService.getScheduleList(meetingNo);
+    }
+
+    @ApiOperation(value = "일정 수정")
+    @ApiImplicitParam(name = "scheduleNo", value = "일정 번호")
     @PutMapping("/schedule/{scheduleNo}")
     public Boolean update(@PathVariable Long scheduleNo, @RequestBody ScheduleUpdateRequestDto requestDto) {
         return scheduleService.update(scheduleNo, requestDto);
     }
 
-    //일정 조회
-    @GetMapping("/schedule")
-    public List<ScheduleResponseDto> getScheduleList() {
-        return scheduleService.getScheduleList();
+    @ApiOperation(value = "일정 삭제")
+    @DeleteMapping("/schedule/{scheduleNo}")
+    public Boolean delete(@PathVariable Long scheduleNo) {
+        return scheduleService.delete(scheduleNo);
     }
+
 }
 
--- src/main/java/com/sswu/meets/domain/schedule/ScheduleRepository.java
+++ src/main/java/com/sswu/meets/domain/schedule/ScheduleRepository.java
@@ -1,5 +1,6 @@
 package com.sswu.meets.domain.schedule;
 
+import com.sswu.meets.domain.meeting.Meeting;
 import com.sswu.meets.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import java.util.List;
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     List<Schedule> findAll();
+    List<Schedule> findByMeeting(Meeting meeting);
 }
