66a048aad0879247473b949b6ac5e2da20a4d7d2
yujung7768903 <yu-jung31476@naver.com>
2022-07-30 15:25:35 +0900

    user api 관련 문서 정리

--- src/main/java/com/sswu/meets/controller/UserController.java
+++ src/main/java/com/sswu/meets/controller/UserController.java
@@ -5,6 +5,8 @@ import com.sswu.meets.dto.UserResponseDto;
 import com.sswu.meets.dto.UserSaveRequestDto;
 import com.sswu.meets.dto.UserUpdateRequestDto;
 import com.sswu.meets.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +15,14 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
+@Api(tags = "유저")
 @RequiredArgsConstructor
 @RestController
 public class UserController {
     private final UserService userService;
     private final HttpSession httpSession;
 
+    @ApiOperation(value = "홈 api", notes = "로그인 한 유저의 경우, OO님 환영합니다. | 로그인 하지 않은 유저의 경우, \"meets에 오신 걸 환영합니다:)\"")
     @GetMapping("/")
     public String hello() {
         SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
@@ -30,19 +34,19 @@ public class UserController {
         }
     }
 
-    // 유저 등록
+    @ApiOperation(value = "유저 등록", notes = "유저 고유 식별 번호 반환")
     @PostMapping("/user")
     public Long save(@RequestBody UserSaveRequestDto userSaveRequestDto) {
         return userService.save(userSaveRequestDto);
     }
 
-    // 모든 유저 정보 조회
+    @ApiOperation(value = "모든 유저 정보 조회")
     @GetMapping("/user")
     public List<UserResponseDto> getUserList() {
         return userService.getUserList();
     }
 
-    // 마이페이지
+    @ApiOperation(value = "마이페이지", notes = "\"로그인 한 유저의 경우, 유저 정보 반환 | 로그인 하지 않은 유저의 경우, \"로그인을 먼저 해주세요.\"")
     @GetMapping("/user/mypage")
     public Object getUserInfo() {
         SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
@@ -53,32 +57,32 @@ public class UserController {
         }
     }
 
-    // 유저 정보 수정
+    @ApiOperation(value = "유저 정보 수정")
     @PutMapping("/user/{user_no}")
     public boolean update(@PathVariable Long user_no, @RequestBody UserUpdateRequestDto userSaveRequestDto){
         return userService.update(user_no, userSaveRequestDto);
     }
 
-    // 유저 정보 삭제
+    @ApiOperation(value = "유저 정보 삭제")
     @DeleteMapping("/user/{user_no}")
     public boolean deleteUser(@PathVariable Long user_no) {
         return userService.deleteUser(user_no);
     }
 
-    // 로그인
+    @ApiOperation(value = "로그인", notes = "구글 로그인 페이지로 이동")
     @GetMapping("/user/login")
     public void login(HttpServletResponse httpServletResponse) throws IOException {
         httpServletResponse.sendRedirect("/oauth2/authorization/google");
     }
 
-    // 로그아웃
+    @ApiOperation(value = "로그아웃", notes = "로그아웃 후 홈 페이지로 이동")
     @GetMapping("/user/logout")
     public String logout() {
         httpSession.invalidate();
         return "redirect:/";
     }
 
-    // 로그인 유무
+    @ApiOperation(value = "로그인 유무", notes = "로그인 한 경우, true 반환 | 로그인 하지 않은 경우, false 반환")
     @GetMapping("/user/status")
     public Boolean getUserStatus() {
         SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
--- src/main/java/com/sswu/meets/dto/UserResponseDto.java
+++ src/main/java/com/sswu/meets/dto/UserResponseDto.java
@@ -1,15 +1,23 @@
 package com.sswu.meets.dto;
 
 import com.sswu.meets.domain.user.User;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 //@NoArgsConstructor
 public class UserResponseDto {
+    @ApiModelProperty(example = "7")
     private Long user_no;
+
+    @ApiModelProperty(example = "yj06283l4@gmail.com")
     private String email;
+
+    @ApiModelProperty(example = "김유정")
     private String name;
+
+    @ApiModelProperty(example = "https://lh3.googleusercontent.com/a-/AFdZucpR_TN-i3qs-t0dQk-LFdHqSIUq_Wvv10iwcAIKSw=s96-c")
     private String profile_url;
 
     public UserResponseDto(User user) {
