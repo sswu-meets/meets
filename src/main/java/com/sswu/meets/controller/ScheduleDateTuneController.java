0aec8b9242373aba585f9a224be5f5115426b921
YoonSeoyeong <l238lwo8@gmail.com>
2022-08-01 01:10:56 +0900

    schedule-date-tune crud 구현

--- src/main/java/com/sswu/meets/controller/ScheduleDateTuneController.java
+++ src/main/java/com/sswu/meets/controller/ScheduleDateTuneController.java
@@ -1,12 +1,13 @@
 package com.sswu.meets.controller;
 
+import com.sswu.meets.dto.ScheduleDateTuneResponseDto;
 import com.sswu.meets.dto.ScheduleDateTuneSaveRequestDto;
+import com.sswu.meets.dto.TodoResponseDto;
 import com.sswu.meets.service.ScheduleDateTuneService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,8 +15,17 @@ public class ScheduleDateTuneController {
     private final ScheduleDateTuneService scheduleDateTuneService;
 
     // 일정 날짜 조율 등록
-   @PostMapping("/scheduleDateTune/{scheduleNo}")
+    @PostMapping("/scheduleDateTune/{scheduleNo}")
     public Long saveDateTune(@PathVariable Long scheduleNo, @RequestBody ScheduleDateTuneSaveRequestDto requestDto) {
         return scheduleDateTuneService.saveDateTune(scheduleNo, requestDto);
     }
+
+
+    // 일정 날짜 조율 조회
+    @GetMapping("/scheduleDateTune/{scheduleNo}")
+    public List<ScheduleDateTuneResponseDto> getDateTuneList(@PathVariable Long scheduleNo) {
+        return scheduleDateTuneService.getDateTuneList(scheduleNo);
+    }
 }
+
+
