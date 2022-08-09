6d128f578c87cba40fb28b1334c187f1704be6ce
YoonSeoyeong <l238lwo8@gmail.com>
2022-08-09 22:41:00 +0900

    모임 프로필 추가

--- src/main/java/com/sswu/meets/dto/MeetingResponseDto.java
+++ src/main/java/com/sswu/meets/dto/MeetingResponseDto.java
@@ -20,10 +20,14 @@ public class MeetingResponseDto {
     @ApiModelProperty(example = "#ED7D31")
     private String meetingColor;
 
+    @ApiModelProperty(example = "https://lh3.googleusercontent.com/a-/AFdZucpR_TN-i3qs-t0dQk-LFdHqSIUq_Wvv10iwcAIKSw=s96-c")
+    private String meetingUrl;      // 일단 user profileUrl을 가져옴. 추후에 수정 예정
+
     public MeetingResponseDto(Meeting meetingEntity) {
         this.meeting_no = meetingEntity.getMeeting_no();
         this.name = meetingEntity.getName();
         this.meetingCode = meetingEntity.getMeetingCode();
         this.meetingColor = meetingEntity.getMeetingColor();
+        this.meetingUrl = meetingEntity.getMeetingUrl();
     }
 }
--- src/main/java/com/sswu/meets/dto/MeetingSaveRequestDto.java
+++ src/main/java/com/sswu/meets/dto/MeetingSaveRequestDto.java
@@ -15,10 +15,14 @@ public class MeetingSaveRequestDto {
     @ApiModelProperty(example = "#ED7D31")
     private String meetingColor;
 
+    @ApiModelProperty(example = "https://lh3.googleusercontent.com/a-/AFdZucpR_TN-i3qs-t0dQk-LFdHqSIUq_Wvv10iwcAIKSw=s96-c")
+    private String meetingUrl;      // 일단 user profileUrl을 가져옴. 추후에 수정 예정
+
     @Builder
-    public MeetingSaveRequestDto(String name, String meetingColor) {
+    public MeetingSaveRequestDto(String name, String meetingColor, String meetingUrl) {
         this.name = name;
         this.meetingColor = meetingColor;
+        this.meetingUrl = meetingUrl;
     }
 
     public Meeting toEntity(String meetingCode) {
@@ -26,6 +30,7 @@ public class MeetingSaveRequestDto {
                 .name(name)
                 .meetingCode(meetingCode)
                 .meetingColor(meetingColor)
+                .meetingUrl(meetingUrl)
                 .build();
     }
 }
