2a798c6132da48918136957a1d8610e0b603e3a2
yujung7768903 <yu-jung31476@naver.com>
2023-01-18 23:51:51 +0900

    ci/cd: 배포 스크립트 추가

---
+++ scripts/deploy.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ec2-user/app/deploy
+PROJECT_NAME=meets
+
+#echo "> Build 파일 복사"
+#cp $REPOSITORY/*.jar
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -f $PROJECT_NAME)
+
+echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+        echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> JAR_NAME 실행"
+
+nohup java -jar \
+        -Dspring.config.location=/home/ec2-user/app/application.properties, classpath:/application-prod.properties \
+        -Dspring.profiled.active=prod \
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
