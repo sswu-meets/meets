3da647e15d7be23ad0c4f7b1c6e51746271d31c5
yujung7768903 <yu-jung31476@naver.com>
2023-01-19 11:51:56 +0900

    test: application-prod.properties 복사 되는지 테스트할 컨트롤러 추가

--- src/main/java/com/sswu/meets/controller/HelloController.java
+++ src/main/java/com/sswu/meets/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.sswu.meets.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class HelloController {
+    @Value("${spring.jpa.database}")
+    private String databaseName;
+
     @GetMapping("/hello")
     public String helloTest() {
         return "hello";
     }
 
+    @GetMapping("/hello/test")
+    public String applicationPropertyTest() {
+        return databaseName;
+    }
 }
