31d0b5dd425ac3d8d8f16dcdb18ca9692b5b7c5b
yujung7768903 <yu-jung31476@naver.com>
2023-01-19 12:13:01 +0900

    ci/cd: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,34 +3,34 @@
 REPOSITORY=/home/ec2-user/app
 PROJECT_NAME=meets
 
-echo "> Build 파일 복사"
+echo "> Copy Build file"
 echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> Check current application pid"
 
 CURRENT_PID=$(pgrep -f $PROJECT_NAME)
 
-echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+echo "Current application pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+        echo "> No applications are currently running"
 else
         echo "> kill -15 $CURRENT_PID"
         kill -15 $CURRENT_PID
         sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> Deploy new application"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가"
+echo "> Add Execution Permission to $JAR_NAME"
 
 chmod +x $JAR_NAME
 
-echo "> JAR_NAME 실행"
+echo "> Run $JAR_NAME"
 
 nohup java -jar \
         -Dspring.config.location=/home/ec2-user/app/application.properties, classpath:/application-prod.properties \
